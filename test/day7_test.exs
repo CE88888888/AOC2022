b0c7e8c1bd7c17f91de05ad15870140b8103c7df
CE88888888 <celbersen@hotmail.com>
2022-12-07 20:12:28 +0100

    Day 7 updated tests

--- test/day7_test.exs
+++ test/day7_test.exs
@@ -2,19 +2,19 @@ defmodule Day7Test do
   use ExUnit.Case
 
   test "day7_1_example" do
-    assert Day7.c(true) == true
+    assert Day7.solve1(true) == 95437
   end
 
-  # test "day7_1" do
-  #   assert Day7.solve1() == false
-  # end
+  test "day7_1" do
+    assert Day7.solve1() == 1749646
+  end
 
-  # test "day7_2_example" do
-  #   assert Day7.solve2(true) == true
-  # end
+  test "day7_2_example" do
+    assert Day7.solve2(true) == 1498966
+  end
 
-  # test "day7_2" do
-  #   assert Day7.solve2() == false
-  # end
+  test "day7_2" do
+    assert Day7.solve2() == 24933642
+  end
 
 end
