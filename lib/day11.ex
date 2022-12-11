5694a54b99e559c89ecd571f157bad8f9f1d2c0a
CE88888888 <celbersen@hotmail.com>
2022-12-11 19:48:12 +0100

    Day 11 - alternative works, how to bench?

--- lib/day11.ex
+++ lib/day11.ex
@@ -48,24 +48,15 @@ defmodule Day11 do
 
   def monkey_parse(monkeystring) do
     list = String.split(monkeystring, "\r\n", trim: true)
-    index = String.to_integer(List.first(Regex.run(~r/(\d+)/, List.first(list), capture: :first)))
-
-    starting_items =
-      Enum.map(Regex.scan(~r/(\d+)/, Enum.at(list, 1), capture: :first), fn [x] -> String.to_integer(x) end)
-
-    operation = List.first(Regex.run(~r/(?>Operation: new = )(.*)/, Enum.at(list, 2), capture: :all_but_first))
-    test = String.to_integer(List.first(Regex.run(~r/(\d+)/, Enum.at(list, 3), capture: :first)))
-    iftrue = String.to_integer(List.first(Regex.run(~r/(\d+)/, Enum.at(list, 4), capture: :first)))
-    iffalse = String.to_integer(List.first(Regex.run(~r/(\d+)/, Enum.at(list, 5), capture: :first)))
 
     %{
       pid: 0,
-      index: index,
-      items: starting_items,
-      testdiv: test,
-      iftrue: iftrue,
-      iffalse: iffalse,
-      operation: operation,
+      index: String.to_integer(List.first(Regex.run(~r/(\d+)/, List.first(list), capture: :first))),
+      items: Enum.map(Regex.scan(~r/(\d+)/, Enum.at(list, 1), capture: :first), fn [x] -> String.to_integer(x) end),
+      testdiv: String.to_integer(List.first(Regex.run(~r/(\d+)/, Enum.at(list, 3), capture: :first))),
+      iftrue: String.to_integer(List.first(Regex.run(~r/(\d+)/, Enum.at(list, 4), capture: :first))),
+      iffalse: String.to_integer(List.first(Regex.run(~r/(\d+)/, Enum.at(list, 5), capture: :first))),
+      operation: List.first(Regex.run(~r/(?>Operation: new = )(.*)/, Enum.at(list, 2), capture: :all_but_first)),
       inspects: 0
     }
   end
@@ -111,119 +102,41 @@ defmodule Day11 do
       "*" -> x1 * x2
     end
   end
-  #-----------------------------Just trying to speed up thing-------------------------------------------
-  def speed_2_example(example \\ false) do
+
+  # -----------------------------Just trying to speed it up---------------------
+
+  def speed_2(example \\ false) do
     input = parse(example)
     mi = Enum.map(0..3, fn _x -> elem(Agent.start_link(fn -> 0 end), 1) end)
     divproduct = Enum.reduce(input, 1, fn x, acc -> x.testdiv * acc end)
-    items = Enum.map(input, fn x -> Enum.map(x.items, fn i -> [x.index, i] end) end) |> Enum.concat()
+    items = Enum.map(input, fn x -> Enum.map(x.items, fn i -> [i, x.index] end) end) |> Enum.concat()
 
-    get_inspects(items, mi, divproduct, 10000)
+    get_inspects(input, items, mi, divproduct, 10000)
     Enum.map(mi, fn x -> Agent.get(x, & &1) end) |> Enum.sort() |> Enum.take(-2) |> Enum.product()
   end
 
-  def get_inspects(items, mi, divproduct, n) do
+  def get_inspects(monkeys, items, mi, divproduct, n) do
     case n do
-      1 -> Enum.map(items, fn [x, y] -> round_calc([x, y], mi, divproduct) end)
-      _ -> get_inspects(Enum.map(items, fn [x, y] -> round_calc([x, y], mi, divproduct) end), mi, divproduct, n - 1)
+      1 -> Enum.map(items, fn [x, y] -> check(x, Enum.at(monkeys, y), monkeys, mi, divproduct) end)
+      _ -> get_inspects(monkeys, Enum.map(items, fn [x, y] -> check(x, Enum.at(monkeys, y), monkeys, mi, divproduct) end), mi, divproduct, n - 1)
     end
   end
 
-  def round_calc([i, x], mi, divproduct) do
-    [ni, nx] =
-      case i do
-        0 ->
-          x = x * 19
-          Agent.update(Enum.at(mi, 0), fn x -> x + 1 end)
-
-          cond do
-            rem(x, 23) != 0 and rem(x + 3, 17) == 0 ->
-              Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-              # 0.3.0
-              [0, x + 3]
-
-            rem(x, 23) != 0 and rem(x + 3, 17) != 0 ->
-              Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-              # 0.3.1
-              [1, x + 3]
-
-            rem(x, 23) == 0 and rem(x * x, 13) == 0 ->
-              Agent.update(Enum.at(mi, 2), fn x -> x + 1 end)
-              # 0.2.1
-              [1, x * x]
-
-            rem(x, 23) == 0 and rem(x * x, 13) != 0 and rem(x * x + 3, 17) == 0 ->
-              Agent.update(Enum.at(mi, 2), fn x -> x + 1 end)
-              Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-              # 0.2.3.0
-              [0, x * x + 3]
-
-            rem(x, 23) == 0 and rem(x * x, 13) != 0 and rem(x * x + 3, 17) != 0 ->
-              Agent.update(Enum.at(mi, 2), fn x -> x + 1 end)
-              Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-              # 0.2.3.1
-              [1, x * x + 3]
-          end
-
-        1 ->
-          x = x + 6
-          Agent.update(Enum.at(mi, 1), fn x -> x + 1 end)
-
-          cond do
-            rem(x, 19) != 0 ->
-              # 0
-              [0, x]
-
-            rem(x, 19) == 0 and rem(x * x, 13) == 0 ->
-              Agent.update(Enum.at(mi, 2), fn x -> x + 1 end)
-              # 2.1
-              [1, x * x]
-
-            rem(x, 19) == 0 and rem(x * x, 13) != 0 and rem(x * x + 3, 17) == 0 ->
-              Agent.update(Enum.at(mi, 2), fn x -> x + 1 end)
-              Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-              # 2.3.0
-              [0, x * x + 3]
-
-            rem(x, 19) == 0 and rem(x * x, 13) != 0 and rem(x * x + 3, 17) != 0 ->
-              Agent.update(Enum.at(mi, 2), fn x -> x + 1 end)
-              Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-              # 2.3.1
-              [1, x * x + 3]
-          end
-
-        2 ->
-          x = x * x
-          Agent.update(Enum.at(mi, 2), fn x -> x + 1 end)
-
-          cond do
-            rem(x, 13) == 0 ->
-              # 1
-              [1, x]
-
-            rem(x, 13) != 0 and rem(x + 3, 17) == 0 ->
-              Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-              # 3.0
-              [0, x + 3]
-
-            rem(x, 13) != 0 and rem(x + 3, 17) != 0 ->
-              Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-              # 3.1
-              [1, x + 3]
-          end
-
-        3 ->
-          x = x + 3
-          Agent.update(Enum.at(mi, 3), fn x -> x + 1 end)
-
-          cond do
-            # 0
-            rem(x, 17) == 0 -> [0, x]
-            # 1
-            rem(x, 17) != 0 -> [1, x]
-          end
-      end
-
-    [ni, rem(nx, divproduct)]
+  def check(x, monkey, monkeys, mi, divproduct) do
+    Agent.update(Enum.at(mi, monkey.index), fn x -> x + 1 end)
+    x = rem(op(monkey.operation, x),divproduct)
+    cond do
+      rem(x, monkey.testdiv) == 0 ->
+        cond do
+          monkey.iftrue < monkey.index -> [x, monkey.iftrue]
+          true -> check(x, Enum.at(monkeys, monkey.iftrue), monkeys, mi, divproduct)
+        end
+      true ->
+        cond do
+          monkey.iffalse < monkey.index -> [x, monkey.iffalse]
+          true -> check(x, Enum.at(monkeys, monkey.iffalse), monkeys, mi, divproduct)
+        end
+    end
   end
+
 end
